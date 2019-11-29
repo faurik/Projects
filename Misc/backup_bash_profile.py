fdcea755a49e30b32565fb8d9641265ffcdad618
faurik <galaktoid1995@gmail.com>
2019-11-29 23:18:56 +0300

    Removed solutions on Python

--- Misc/backup_bash_profile.py
+++
@@ -1,6 +0,0 @@
-import shutil
-
-bash_file_path = '/Users/migueloliveira/.bash_profile'
-dest_file_path = '/Users/migueloliveira/Dropbox/Stuff/.bash_profile_backup'
-
-shutil.copyfile(bash_file_path, dest_file_path)
